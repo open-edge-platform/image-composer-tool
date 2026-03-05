cdb3b594a4cac65274ba74e44c5404dffba38ccb
samueltaripin <samuel.taripin@intel.com>
2026-03-05 13:53:04 +0800

    fix grub has no initrd

--- internal/image/imageboot/imageboot.go
+++ internal/image/imageboot/imageboot.go
@@ -189,7 +189,6 @@ func updateInitramfsForGrub(installRoot, kernelVersion string, template *config.
 		}
 	} else {
 		log.Debugf("No extra modules specified in enableExtraModules")
-		return nil
 	}
 
 	updateInitramfsExists, err := shell.IsCommandExist("update-initramfs", installRoot)
